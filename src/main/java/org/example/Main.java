161790ac3f9e344d13931074bc619e97b0718847
Denis Damshel <denisdamshel@gmail.com>
2023-07-12 10:45:54 +0300

    create and add 2 users in db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
@@ -11,7 +14,18 @@ public class Main {
         configuration.configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("ok");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("vanya@gmail.ru")
+                    .firstname("Ivan")
+                    .lastname("Petrov")
+                    .birthDate(LocalDate.of(1999,1,20))
+                    .age(24)
+                    .build();
+            session.persist(user);
+
+            session.getTransaction().commit();
         }
     }
 }
