d3a1a7c45d7d8ab5710cedddcb2bf7b55d730dc4
Denis Damshel <denisdamshel@gmail.com>
2023-07-12 09:41:40 +0300

    create session

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args){
+        Configuration configuration = new Configuration();
+
+        configuration.configure();
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("ok");
+        }
+    }
+}
